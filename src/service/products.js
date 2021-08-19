20ec4659f16fb2f57d2571ed37360ebd58557f64
AmmukhanAk <amerkhanak007@gmail.com>
2021-08-19 03:42:22 -0700

    update
    by ammu

--- src/service/products.js
+++ src/service/products.js
@@ -4,5 +4,7 @@ export const getProducts=()=>{
     axios
     .get("https://fakestoreapi.com/products")
    .then((res)=> (allProducts=[...res.data]))
-   .catch((e)=> console.log("Error-",e))
-}~
+   .catch((e)=> console.log("Error-",e));
+
+   return allProducts;
+};

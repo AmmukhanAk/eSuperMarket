bdf83218520db7157f76e2684ca3da8021a12af7
AmmukhanAk <amerkhanak007@gmail.com>
2021-08-19 03:33:51 -0700

    updating
    ammu

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import React,{useEffect} from "react";
 import {Provider} from "react-redux"
 import { configStore } from "./state/store/configStore";
 import logo from './logo.svg';
 import './App.css';
 import {Home} from "./components/Home.jsx"
+import {getProducts}from "./service/products"
+
 function App() {
   const LocalStore = configStore()
+useEffect(()=>{
+    const products=getProducts();
+},[])
   return (
     <div>
       <Provider store={LocalStore}>
---
+++ src/service/products.js
@@ -0,0 +1,8 @@
+import axios from "axios"
+export const getProducts=()=>{
+  let allProducts=[];
+    axios
+    .get("https://fakestoreapi.com/products")
+   .then((res)=> (allProducts=[...res.data]))
+   .catch((e)=> console.log("Error-",e))
+}~

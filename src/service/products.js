43f55aba3042a50a62a9cbfc305b5fcda098840b
AmmukhanAk <amerkhanak007@gmail.com>
2021-08-19 03:50:47 -0700

    update
    final

--- src/service/products.js
+++ src/service/products.js
@@ -1,10 +1,14 @@
 import axios from "axios"
-export const getProducts=()=>{
-  let allProducts=[];
-    axios
-    .get("https://fakestoreapi.com/products")
-   .then((res)=> (allProducts=[...res.data]))
-   .catch((e)=> console.log("Error-",e));
 
-   return allProducts;
-};
+export const getProducts= async()=>{
+    const resp = await axios.get("https://fakestoreapi.com/products")
+    resp.data
+
+
+  
+//     axios
+//     .get("https://fakestoreapi.com/products")
+//    .then((res)=>
+//     ((allProducts=[...res.data]))
+//    .catch((e)=> console.log("Error-",e))
+// };
